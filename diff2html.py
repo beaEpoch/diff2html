ab31eede61b15b52fb3e0313bcc8d758bb27c3b4
beaEpoch <du.hrbeu@gmail.com>
2017-05-18 16:24:07 +0800

    Create diff2html.py

---
+++ diff2html.py
@@ -0,0 +1,379 @@
+#! /usr/bin/python
+# coding=utf-8
+
+import sys, re, htmlentitydefs, getopt, StringIO, codecs, datetime
+
+# minimum line size, we add a zero-sized breakable space every
+# LINESIZE characters
+linesize = 20
+tabsize = 8
+show_CR = False
+encoding = "utf-8"
+lang = "en"
+algorithm = 0
+
+desc = "File comparison"
+dtnow = datetime.datetime.now()
+modified_date = "%s+01:00"%dtnow.isoformat()
+
+DIFFON = "\x01"
+DIFFOFF = "\x02"
+
+buf = []
+add_cpt, del_cpt = 0, 0
+line1, line2 = 0, 0
+hunk_off1, hunk_size1, hunk_off2, hunk_size2 = 0, 0, 0, 0
+
+
+# Characters we're willing to word wrap on
+WORDBREAK = " \t;.,/):-"
+
+def sane(x):
+    r = ""
+    for i in x:
+        j = ord(i)
+        if i not in ['\t', '\n'] and (j < 32):
+            r = r + " "
+        else:
+            r = r + i
+    return r
+
+def linediff(s, t):
+    '''
+    Original line diff algorithm of diff2html. It's character based.
+    '''
+    if len(s):
+        s = unicode(reduce(lambda x, y:x+y, [ sane(c) for c in s ]))
+    if len(t):
+        t = unicode(reduce(lambda x, y:x+y, [ sane(c) for c in t ]))
+
+    m, n = len(s), len(t)
+    d = [[(0, 0) for i in range(n+1)] for i in range(m+1)]
+
+
+    d[0][0] = (0, (0, 0))
+    for i in range(m+1)[1:]:
+        d[i][0] = (i,(i-1, 0))
+    for j in range(n+1)[1:]:
+        d[0][j] = (j,(0, j-1))
+
+    for i in range(m+1)[1:]:
+        for j in range(n+1)[1:]:
+            if s[i-1] == t[j-1]:
+                cost = 0
+            else:
+                cost = 1
+            d[i][j] = min((d[i-1][j][0] + 1, (i-1, j)),
+                          (d[i][j-1][0] + 1, (i, j-1)),
+                          (d[i-1][j-1][0] + cost, (i-1, j-1)))
+
+    l = []
+    coord = (m, n)
+    while coord != (0, 0):
+        l.insert(0, coord)
+        x, y = coord
+        coord = d[x][y][1]
+
+    l1 = []
+    l2 = []
+
+    for coord in l:
+        cx, cy = coord
+        child_val = d[cx][cy][0]
+
+        father_coord = d[cx][cy][1]
+        fx, fy = father_coord
+        father_val = d[fx][fy][0]
+
+        diff = (cx-fx, cy-fy)
+
+        if diff == (0, 1):
+            l1.append("")
+            l2.append(DIFFON + t[fy] + DIFFOFF)
+        elif diff == (1, 0):
+            l1.append(DIFFON + s[fx] + DIFFOFF)
+            l2.append("")
+        elif child_val-father_val == 1:
+            l1.append(DIFFON + s[fx] + DIFFOFF)
+            l2.append(DIFFON + t[fy] + DIFFOFF)
+        else:
+            l1.append(s[fx])
+            l2.append(t[fy])
+
+    r1, r2 = (reduce(lambda x, y:x+y, l1), reduce(lambda x, y:x+y, l2))
+    return r1, r2
+
+
+def diff_changed(old, new):
+    '''
+    Returns the differences basend on characters between two strings
+    wrapped with DIFFON and DIFFOFF using `diff`.
+    '''
+    con = {'=': (lambda x: x),
+           '+': (lambda x: DIFFON + x + DIFFOFF),
+           '-': (lambda x: '')}
+    return "".join([(con[a])("".join(b)) for a, b in diff(old, new)])
+
+
+def diff_changed_ts(old, new):
+    '''
+    Returns a tuple for a two sided comparison based on characters, see `diff_changed`.
+    '''
+    return (diff_changed(new, old), diff_changed(old, new))
+
+
+def word_diff(old, new):
+    '''
+    Returns the difference between the old and new strings based on words. Punctuation is not part of the word.
+
+    Params:
+        old the old string
+        new the new string
+
+    Returns:
+        the output of `diff` on the two strings after splitting them
+        on whitespace (a list of change instructions; see the docstring
+        of `diff`)
+    '''
+    separator_pattern = '(\W+)';
+    return diff(re.split(separator_pattern, old, flags=re.UNICODE), re.split(separator_pattern, new, flags=re.UNICODE))
+
+
+def diff_changed_words(old, new):
+    '''
+    Returns the difference between two strings based on words (see `word_diff`)
+    wrapped with DIFFON and DIFFOFF.
+
+    Returns:
+        the output of the diff expressed delimited with DIFFON and DIFFOFF.
+    '''
+    con = {'=': (lambda x: x),
+           '+': (lambda x: DIFFON + x + DIFFOFF),
+           '-': (lambda x: '')}
+    return "".join([(con[a])("".join(b)) for a, b in word_diff(old, new)])
+
+
+def diff_changed_words_ts(old, new):
+    '''
+    Returns a tuple for a two sided comparison based on words, see `diff_changed_words`.
+    '''
+    return (diff_changed_words(new, old), diff_changed_words(old, new))
+
+
+def convert(s, linesize=0, ponct=0):
+    i = 0
+    t = u""
+    for c in s:
+        # used by diffs
+        if c == DIFFON:
+            t += u'<span class="diffchanged2">'
+        elif c == DIFFOFF:
+            t += u"</span>"
+
+        # special html chars
+        elif htmlentitydefs.codepoint2name.has_key(ord(c)):
+            t += u"&%s;" % (htmlentitydefs.codepoint2name[ord(c)])
+            i += 1
+
+        # special highlighted chars
+        elif c == "\t" and ponct == 1:
+            n = tabsize-(i%tabsize)
+            if n == 0:
+                n = tabsize
+            t += (u'<span class="diffponct">&raquo;</span>'+'&nbsp;'*(n-1))
+        #elif c == " " and ponct == 1:
+        #    t += u'<span class="diffponct">&middot;</span>'
+        elif c == "\n" and ponct == 1:
+            if show_CR:
+                t += u'<span class="diffponct">\</span>'
+        else:
+            t += c
+            i += 1
+
+        if linesize and (WORDBREAK.count(c) == 1):
+            t += u'&#8203;'
+            i = 0
+        if linesize and i > linesize:
+            i = 0
+            t += u"&#8203;"
+
+    return t
+
+
+def add_collapsable(s, output_file):
+    output_file.write(('<div class="diffmisc"><td colspan="4">%s</td>\n'%convert(s)).encode(encoding))
+
+
+def add_filename(output_file):
+    output_file.write("<tr><th class='difftablehead' colspan='2'>修改前</th>")
+    output_file.write("<th class='difftablehead' colspan='2'>修改后</th></tr>\n")
+
+
+def add_hunk(output_file, show_hunk_infos):
+    if show_hunk_infos:
+        output_file.write('<tr class="diffhunk"><td colspan="2">Offset %d, %d lines modified</td>'%(hunk_off1, hunk_size1))
+        output_file.write('<td colspan="2">Offset %d, %d lines modified</td></tr>\n'%(hunk_off2, hunk_size2))
+    else:
+        # &#8942; - vertical ellipsis
+        output_file.write('<tr class="diffhunk"><td colspan="2">&#8942;</td><td colspan="2">&#8942;</td></tr>')
+
+
+def add_line(s1, s2, output_file):
+    global line1
+    global line2
+
+    orig1 = s1
+    orig2 = s2
+
+    if s1 == None and s2 == None:
+        type_name = "unmodified"
+    elif s1 == None or s1 == "":
+        type_name = "added"
+    elif s2 == None or s1 == "":
+        type_name = "deleted"
+    elif s1 == s2:
+        type_name = "unmodified"
+    else:
+        type_name = "changed"
+        if algorithm == 1:
+            s1, s2 = diff_changed_words_ts(orig1, orig2)
+        elif algorithm == 2:
+            s1, s2 = diff_changed_ts(orig1, orig2)
+        else: # default
+            s1, s2 = linediff(orig1, orig2)
+
+    output_file.write(('<tr class="diff%s">' % type_name).encode(encoding))
+    if s1 != None and s1 != "":
+        output_file.write(('<td class="diffline">%d </td>' % line1).encode(encoding))
+        output_file.write('<td class="diffpresent">'.encode(encoding))
+        output_file.write(convert(s1, linesize=linesize, ponct=1).encode(encoding))
+        output_file.write('</td>')
+    else:
+        s1 = ""
+        output_file.write('<td colspan="2"> </td>')
+
+    if s2 != None and s2 != "":
+        output_file.write(('<td class="diffline">%d </td>'%line2).encode(encoding))
+        output_file.write('<td class="diffpresent">')
+        output_file.write(convert(s2, linesize=linesize, ponct=1).encode(encoding))
+        output_file.write('</td>')
+    else:
+        s2 = ""
+        output_file.write('<td colspan="2"></td>')
+
+    output_file.write('</tr>\n')
+
+    if s1 != "":
+        line1 += 1
+    if s2 != "":
+        line2 += 1
+
+
+def empty_buffer(output_file):
+    global buf
+    global add_cpt
+    global del_cpt
+
+    if del_cpt == 0 or add_cpt == 0:
+        for l in buf:
+            add_line(l[0], l[1], output_file)
+
+    elif del_cpt != 0 and add_cpt != 0:
+        l0, l1 = [], []
+        for l in buf:
+            if l[0] != None:
+                l0.append(l[0])
+            if l[1] != None:
+                l1.append(l[1])
+        max_len = (len(l0) > len(l1)) and len(l0) or len(l1)
+        for i in range(max_len):
+            s0, s1 = "", ""
+            if i < len(l0):
+                s0 = l0[i]
+            if i < len(l1):
+                s1 = l1[i]
+            add_line(s0, s1, output_file)
+
+    add_cpt, del_cpt = 0, 0
+    buf = []
+
+
+def parse_input(input_file, output_file, input_file_name, output_file_name,
+                show_hunk_infos):
+    global add_cpt, del_cpt
+    global line1, line2
+    global hunk_off1, hunk_size1, hunk_off2, hunk_size2
+    table_flag = False
+    filenum = 0
+
+    while True:
+        l = input_file.readline()
+        # input_file content must have no blank line
+        if l == "":
+            break
+
+        m = re.match('^diff -ru\s+(\S+)\s+(\S+)', l)
+        if m:
+            filenum += 1
+            empty_buffer(output_file)
+            filename = m.group(2)
+            if table_flag:
+                output_file.write(('</table></div>').encode(encoding))
+            else:
+                table_flag = True
+            add_collapsable(filename, filenum, output_file)
+            output_file.write(('<table class="diff_exp_table">').encode(encoding))
+            continue
+
+        m = re.match('^--- ([^\s]*)', l)
+        if m:
+            empty_buffer(output_file)
+            file1 = m.groups()[0]
+            while True:
+                l = input_file.readline()
+                m = re.match('^\+\+\+ ([^\s]*)', l)
+                if m:
+                    file2 = m.groups()[0]
+                    break
+            add_filename(output_file)
+            hunk_off1, hunk_size1, hunk_off2, hunk_size2 = 0, 0, 0, 0
+            continue
+
+        m = re.match("@@ -(\d+),?(\d*) \+(\d+),?(\d*)", l)
+        if m:
+            empty_buffer(output_file)
+            hunk_data = map(lambda x:x=="" and 1 or int(x), m.groups())
+            hunk_off1, hunk_size1, hunk_off2, hunk_size2 = hunk_data
+            line1, line2 = hunk_off1, hunk_off2
+            add_hunk(output_file, show_hunk_infos)
+            continue
+
+        if re.match("^\+", l):
+            add_cpt += 1
+            hunk_size2 -= 1
+            buf.append((None, l[1:]))
+            continue
+
+        if re.match("^\-", l):
+            del_cpt += 1
+            hunk_size1 -= 1
+            buf.append((l[1:], None))
+            continue
+
+        if re.match("^\ ", l) and hunk_size1 and hunk_size2:
+            empty_buffer(output_file)
+            hunk_size1 -= 1
+            hunk_size2 -= 1
+            buf.append((l[1:], l[1:]))
+            continue
+
+    empty_buffer(output_file)
+    output_file.write(('</table></div>').encode(encoding))
+    output_file.write(('<br /><div class="notemessage"><font color="#880000">NOTE:</font> %d File(s) Are Changed.</div>'%filenum).encode(encoding))
+
+def parse_from_memory(txt, show_hunk_infos):
+    " Parses diff from memory and returns a string with html "
+    input_stream = StringIO.StringIO(txt)
+    output_stream = StringIO.StringIO()
+    parse_input(input_stream, output_stream, '', '', show_hunk_infos)
+    return output_stream.getvalue()
